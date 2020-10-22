99e7336cf46ea617f75ef9d9202ade88bb0d3345
William Easter <williambeaster@gmail.com>
2020-10-22 23:38:01 +0900

    testing in process

--- week1/lab/lab1.py
+++ week1/lab/lab1.py
@@ -2,7 +2,8 @@
 
 
 #according to wikipedia, this is the age of the oldest living Ameircan
-OLDEST_AGE = 115
+#I add one year because 115.1 is > 115 but is still valid.
+OLDEST_AGE = 116
 STATE_CODES = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", 
     "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", 
     "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", 
@@ -12,6 +13,10 @@ GREATEST_ZIP = "99950"
 LEAST_ZIP = "000501"
 
 def queryContinue():
+    """
+    DRY for asking whether the user wants to continue
+    Since we ask after every question, this should be a function
+    """
     print("Continue? (y/n): ")
     proceed = input()
     
@@ -43,11 +48,18 @@ def main():
 
     # and not isinstance(age, int)):
     while (age < 0 or age > OLDEST_AGE):
-        print(f"Age (0 - {OLDEST_AGE}):", end=" ")
+        print(f"Age (18 - {OLDEST_AGE}):", end=" ")
         age = int(input())
+    if (age < 18):
+        print("You are too young to vote. Exiting program.")
+        exit(0)
     queryContinue()
 
     citizen = input("Are you a US citizen (y/n): ")
+    if (citizen != 'y'):
+        exit(0)
+    #TODO: make a variable yes = ['y', 'Y', 'yes', 'YES"] and test for
+    #in that set to continue
     queryContinue()
 
     while (not (state in STATE_CODES)):

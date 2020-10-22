27354f6db4100306b9fd192a20389062b15c9e97
William Easter <williambeaster@gmail.com>
2020-10-22 14:41:09 +0900

    final

--- week1/discussion/discussion.py
+++ week1/discussion/discussion.py
@@ -4,12 +4,19 @@ This just lets you type in a string and then prints it out in a diagonal
 line. If one of the characters is "W" then there will be one additional
 space before the following character.
 """
-name = input("Name: ")
+def main():
+    """
+    This function exists solely so pylint does not flag 'space' as a
+    constant
+    """
+    name = input("Name: ")
+    space = " "
 
-space = " "
-for char in name:
-    print(space, char)
-    if char == "W":
-        space += "  "
-    else:
-        space += " "
+    for char in name:
+        print(space, char)
+        if char == "W":
+            space += "  "
+        else:
+            space += " "
+
+main()

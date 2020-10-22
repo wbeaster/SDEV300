535514f108b3bb525548c132d2e0df74ce5924d6
William Easter <williambeaster@gmail.com>
2020-10-22 14:25:58 +0900

    in progress

--- week1/discussion/discussion.py
+++ week1/discussion/discussion.py
@@ -1,1 +1,15 @@
 #test
+"""
+This just lets you type in a string and then prints it out in a diagonal
+line. If one of the characters is "W" then there will be one additional
+space before the following character.
+"""
+name = input("Name: ")
+
+space = " "
+for char in name:
+    print(space, char)
+    if char == "W":
+        space += "  "
+    else:
+        space += " "

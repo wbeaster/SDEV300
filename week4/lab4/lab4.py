06a8aedb106393c3437c2f65bb147ff0f8a906e3
William Easter <williambeaster@gmail.com>
2020-11-13 22:08:31 +0900

    first attempt: zip, phone, matrix

--- week4/lab4/lab4.py
+++ week4/lab4/lab4.py
@@ -1,4 +1,10 @@
+import re
 
+import numpy as np
+import pandas as pd
+
+REGEX_PHONE = r"\d{3}-\d{3}-\d{4}"
+REGEX_ZIP4 = r"\d{5}-\d{4}"
 
 def get_phone():
     print('Please enter your phone number in this form:')
@@ -14,60 +20,137 @@ def get_zip4():
 
     return zip4
 
-def get_matrix():
+def get_row(num):
+    row = None
+    while not row:
+        row = input(f"Matrix row {num}: ")
+        row = row.split()
+        if len(row) != 3:
+            row = None
+        else:
+            for element in row:
+                try:
+                    element = float(element)
+                except ValueError:
+                    row = None
+    return row
 
-    #Show the matrix grid, including the column and row IDs
-    #Then:
-    #   Enter A1
-    #   Enter A2
-    #      ...
-    #   Enter C3
-    #Allow Decimals
+def get_matrices():
 
-    print('This is what your 3 x 3 matrix looks like:')
-    print('   1   2   3')
-    print('A  x   x   x')
-    print('B  x   x   x')
-    print('C  x   x   x')
-    print('You will be asked to input your matrix one element at a time.')
-    print('Inputs must be numbers')
-    
+    #TODO: Clean up this language
+    print("This is where the detailed instruction go (lenght, integer/float, etc")
 
-    return
+    print("This is for matrix 1:")
+    row1 = get_row(1)
+    row2 = get_row(2)
+    row3 = get_row(3)
+    matrix1 = np.array([row1, row2, row3], dtype=np.float64)
+
+    print("This is for matrix 2:")
+    row1 = get_row(1)
+    row2 = get_row(2)
+    row3 = get_row(3)
+    matrix2 = np.array([row1, row2, row3], dtype=np.float64)
+
+    print("Here's matrix 1:")
+    print(matrix1)
+
+    print("Here's matrix 2:")
+    print(matrix2)
+
+
+    return matrix1, matrix2
 
 def handle_phone():
-    phone_number = None
+    phone_number = '-1'
 
     while not is_valid_phone(phone_number):
         phone_number = get_phone()
 
+    print('Valid phone number. Thank you.')
+
     return
 
 def handle_zip4():
-    zip4 = None
+    zip4 = '-1'
 
     while not is_valid_zip4(zip4):
         zip4 = get_zip4()
 
+    print('Valid ZIP+4. Thank you.')
+
     return
 
-def handle_matrix()
-    valid_matrix = False
-    matrix = None
+def handle_matrix():
+    #matrix1, matrix2 = get_matrices()
+    matrix1 = np.array([[1, 2, 4],
+                        [4, 2, 1],
+                        [3, 8, 9]])
 
-    while not valid_matrix:
+    matrix2 = np.array([[3, 2, 1],
+                        [7, 2, 5],
+                        [5, 2, 1]])
+
+    choice = None
+    #TODO: Jsut added this, need to test it
+    #help from here: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
+    np.set_printoptions(precision=3)
+
+    while choice != 4:
+        print("***Okay, let's have some fun with those matrices!***")
+        print("1. Add")
+        print("2. Subtract")
+        print("3. Matrix multiplication")
+        print("4. Element by element multiplication")
+        print("5. Exit")
+        choice = input("Please enter a number (1-4): ")
+
+        #TODO: Figure out he phraseology between matrix multiplication and element multiplication
+        if choice == "1":
+            matrix3 = matrix1 + matrix2
+            print(matrix1)
+            print("+")
+            print(matrix2)
+            print("=")
+            print(matrix3)
+            print("Transpose:")
+            print(matrix3.T)
+            #TODO: Round to the thousandths please
+            print("The mean of rows:")
+            print(matrix3.mean(axis=1))
+            print("The mean of columns:")
+            print(matrix3.mean(axis=0))
+        elif choice == "2":
+            matrix3 = matrix1 - matrix2
+            print(matrix1)
+            print("-")
+            print(matrix2)
+            print("=")
+            print(matrix3)
+        elif choice == "3":
+            matrix3 = np.matmul(matrix1, matrix2)
+            print(matrix1)
+            print("*")
+            print(matrix2)
+            print("=")
+            print(matrix3)
+        elif choice == "4":
+            matrix3 = matrix1 * matrix2
+            print(matrix1)
+            print("*")
+            print(matrix2)
+            print("=")
+            print(matrix3)
+        elif choice == '5':
+            print("Thank you for using this application")
+            break
+    return
         
-
-
-
 def is_valid_phone(number):
-    #TODO: Do the regex thing here
-
-    return
+    return re.fullmatch(REGEX_PHONE, number)
 
 def is_valid_zip4(zip4):
-    #TODO: Do the regext hing here
-    return
+    return re.fullmatch(REGEX_ZIP4, zip4)
 
 def main_menu():
     choice = None
@@ -78,7 +161,7 @@ def main_menu():
         print('2. Validate ZIP+4')
         print('3. Matrix math')
         print('4. Exit')
-        input('Please enter a number (1-4): ')
+        choice = input('Please enter a number (1-4): ')
 
         if choice == '1':
             handle_phone()
@@ -88,9 +171,12 @@ def main_menu():
             handle_matrix()
         elif choice == '4':
             print("Thank you for using this application")
+            break
 
     return
 
 def main():
     main_menu()
     return
+
+main()

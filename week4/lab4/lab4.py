63b2caf7a95ea60fdaa99347584452c3b4d70e97
William Easter <williambeaster@gmail.com>
2020-11-14 10:26:18 +0900

    ready for testing

--- week4/lab4/lab4.py
+++ week4/lab4/lab4.py
@@ -1,12 +1,25 @@
-import re
+"""
+Lab3 allows auser to enter and validatetheir phone number and zipcode+4. Then
+the user will entervalues of two, 3x3 matrices and then select from options
+including, addition, subtraction, matrix multiplication, and element by element
+multiplication. You should use numpy.matmul()for matrix multiplication (e.g.
+np.matmul(a, b)).The program should computethe appropriate results and return
+the results, the transpose of the results, the mean of the rows for the
+results, and the mean of the columns for the results.
+"""
 
+import re
 import numpy as np
-import pandas as pd
 
 REGEX_PHONE = r"\d{3}-\d{3}-\d{4}"
 REGEX_ZIP4 = r"\d{5}-\d{4}"
 
 def get_phone():
+    """
+    Parameters: None
+
+    Returns: A number which may or may not be a valid phone number
+    """
     print('Please enter your phone number in this form:')
     print('XXX-XXX-XXXX')
     number = input()
@@ -14,6 +27,11 @@ def get_phone():
     return number
 
 def get_zip4():
+    """
+    Parameters: None
+
+    Returns: A number which may or may not be a valid zip+4
+    """
     print('Please enter your ZIP+4 in this form:')
     print('XXXXX-XXXX')
     zip4 = input()
@@ -21,6 +39,12 @@ def get_zip4():
     return zip4
 
 def get_row(num):
+    """
+    Parameters: num, which is the number of the row that will be displayed
+    to the user
+
+    Returns: A list of three floats
+    """
     row = None
     while not row:
         row = input(f"Matrix row {num}: ")
@@ -36,9 +60,16 @@ def get_row(num):
     return row
 
 def get_matrices():
+    """
+    Parameters: None
 
-    #TODO: Clean up this language
-    print("This is where the detailed instruction go (lenght, integer/float, etc")
+    Returns: A tuple of two 3x3 matrices of floats
+    """
+
+    print("Please enter two matrices.")
+    print("The entries will be one row at a time.")
+    print("The elements will be seperated by spaces.")
+    print("Entries must be a real number.")
 
     print("This is for matrix 1:")
     row1 = get_row(1)
@@ -62,6 +93,14 @@ def get_matrices():
     return matrix1, matrix2
 
 def handle_phone():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function is the controller for obtaining and validating a
+    phone number
+    """
     phone_number = '-1'
 
     while not is_valid_phone(phone_number):
@@ -72,6 +111,14 @@ def handle_phone():
     return
 
 def handle_zip4():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function is the controller for obtaining and validating a
+    zip+4
+    """
     zip4 = '-1'
 
     while not is_valid_zip4(zip4):
@@ -82,7 +129,18 @@ def handle_zip4():
     return
 
 def handle_matrix():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function does control over the matrix portion of the program.
+    It calls the function that gets the matrices from the user. Then
+    it displays a menu to the user to let the user determine what
+    operation to perform on the matrices.
+    """
     #matrix1, matrix2 = get_matrices()
+    #TODO: Delete this test data before you submit!
     matrix1 = np.array([[1, 2, 4],
                         [4, 2, 1],
                         [3, 8, 9]])
@@ -92,9 +150,12 @@ def handle_matrix():
                         [5, 2, 1]])
 
     choice = None
-    #TODO: Jsut added this, need to test it
-    #help from here: https://stackoverflow.com/questions/2891790/how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
-    np.set_printoptions(precision=3)
+
+    #help from here: https://stackoverflow.com/questions/2891790/
+    # how-to-pretty-print-a-numpy-array-without-scientific-notation-and-with-given-pre
+    #https://numpy.org/doc/stable/reference/generated/
+    # numpy.set_printoptions.html#numpy.set_printoptions
+    np.set_printoptions(precision=2, floatmode='fixed')
 
     while choice != 4:
         print("***Okay, let's have some fun with those matrices!***")
@@ -105,7 +166,6 @@ def handle_matrix():
         print("5. Exit")
         choice = input("Please enter a number (1-4): ")
 
-        #TODO: Figure out he phraseology between matrix multiplication and element multiplication
         if choice == "1":
             matrix3 = matrix1 + matrix2
             print(matrix1)
@@ -113,13 +173,6 @@ def handle_matrix():
             print(matrix2)
             print("=")
             print(matrix3)
-            print("Transpose:")
-            print(matrix3.T)
-            #TODO: Round to the thousandths please
-            print("The mean of rows:")
-            print(matrix3.mean(axis=1))
-            print("The mean of columns:")
-            print(matrix3.mean(axis=0))
         elif choice == "2":
             matrix3 = matrix1 - matrix2
             print(matrix1)
@@ -128,6 +181,7 @@ def handle_matrix():
             print("=")
             print(matrix3)
         elif choice == "3":
+            print("Matrix multiplication")
             matrix3 = np.matmul(matrix1, matrix2)
             print(matrix1)
             print("*")
@@ -135,6 +189,7 @@ def handle_matrix():
             print("=")
             print(matrix3)
         elif choice == "4":
+            print("Matrix element multiplication")
             matrix3 = matrix1 * matrix2
             print(matrix1)
             print("*")
@@ -144,15 +199,43 @@ def handle_matrix():
         elif choice == '5':
             print("Thank you for using this application")
             break
+
+        print("Transpose:")
+        print(matrix3.T)
+        print("The mean of rows:")
+        print(matrix3.mean(axis=1))
+        print("The mean of columns:")
+        print(matrix3.mean(axis=0))
+
     return
-        
+
 def is_valid_phone(number):
+    """
+    Parameters: A string
+
+    Returns: True if the string is a regex match for a phone mumber per
+    the constant REGEX_PHONE
+    """
     return re.fullmatch(REGEX_PHONE, number)
 
 def is_valid_zip4(zip4):
+    """
+    Parameters: A string
+
+    Returns: True if the string is a regex match for zip+4 per the constant
+    REGEX_ZIP4
+    """
     return re.fullmatch(REGEX_ZIP4, zip4)
 
 def main_menu():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function display the main menu and then call the appropriate
+    routines based on the user unput
+    """
     choice = None
 
     while choice != 4:
@@ -176,6 +259,13 @@ def main_menu():
     return
 
 def main():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This is just a stub that calls main_menu()
+    """
     main_menu()
     return
 

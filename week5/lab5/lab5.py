2417815eedc43e81fbc6c6c18e9a5cd8a1e4fe87
William Easter <williambeaster@gmail.com>
2020-11-20 22:39:21 +0900

    mostly finished

--- week5/lab5/lab5.py
+++ week5/lab5/lab5.py
@@ -1,12 +1,27 @@
+"""
+This is SDEV300 7384 Lab 5 - Python Data Analysis
+
+This program will read csv fiels and then display Pandas generated
+information from the contents of the file.
+"""
+
 import os
 
 from matplotlib import pyplot as plt
 import pandas as pd
 
-LOC_POP_CHANGE = "/week5/lab5/PopChange.csv"
+# TODO: use the data without Puerto Rico?
+LOC_POP_CHANGE = "/week5/lab5/PopChange2.csv"
 LOC_HOUSING = "/week5/lab5/Housing.csv"
 
 def data_dump(df, column):
+    """
+    Parameters:
+    df = Pandas data frame with the
+    column = string of the column whose data will be displayed
+
+    Returns: Nothing
+    """
     print(f"You selected {column}")
     print("The statistics for this column are:")
     print(df[column].describe())
@@ -15,16 +30,21 @@ def data_dump(df, column):
     plt.show()
 
 def analyze_housing_data():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function displays a menu to the user, asks for their selection
+    and then calls a function to display the appropriate data.
+    """
+
     #open the file
     pd.set_option('precision', 2)
     pd.set_option('display.float_format', '{:,.2f}'.format)
     df = pd.read_csv(os.getcwd() + LOC_HOUSING)
-    #df.columns = ["id", "geography", "tgi", "tgi2", "pop_apr1", "pop_jul1", "delta"]
-    print(df.columns)
-    print(df.head())
-    print(df.tail())
-    
-    #show a menu givning the user the choice on which column to analyze
+
+    #show a menu giving the user the choice on which column to analyze
     print("You selected population data")
 
     choice = None
@@ -44,12 +64,6 @@ def analyze_housing_data():
 
         if choice == "1":
             data_dump(df, "AGE")
-            # print("You selected _____")
-            # print("The statistics for this column are:")
-            # print(df["AGE"].describe())
-            # plt.hist(df["AGE"], edgecolor='black')
-            # print("The histogram for this column is now displayed")
-            # plt.show()
             choice = None
         elif choice == "2":
             data_dump(df, "BEDRMS")
@@ -74,61 +88,63 @@ def analyze_housing_data():
             break
         else:
             choice = None
-    
-    #spit out the stats and the histogram
-    #show the menu again
     return
 
 def analyze_population_data():
-    #open the file
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function displays a menu to the user, asks for their selection
+    and then calls a function to display the appropriate data.
+    """
+
     #TODO: Make it so it does not output exponential notation
-    #TODO: Best video for 
     pd.set_option('precision', 2)
     pd.set_option('display.float_format', '{:,.2f}'.format)
     df = pd.read_csv(os.getcwd() + LOC_POP_CHANGE)
     df.columns = ["id", "geography", "tgi", "tgi2", "pop_apr1", "pop_jul1", "delta"]
-    
-    #show a menu givning the user the choice on which column to analyze
+
     print("You selected population data")
 
     choice = None
 
     while not choice:
         print("Select your choice:")
-        print("1. Analyze 'Pop Apr 1'")
-        print("2. Analyze 'Pop Jul 1'")
-        print("3. Analyze 'Change pop'")
+        print("1. Analyze 'pop_apr1'")
+        print("2. Analyze 'pop_jul1'")
+        print("3. Analyze 'delta' (change in population over time)")
         print("4. Exit")
 
         choice = input("Enter your choice (1 - 4): ")
 
         if choice == "1":
-            print("You selected _____")
-            print("The statistics for this column are:")
-            print(df["pop_apr1"].describe())
-            plt.hist(df["pop_apr1"], log=True)
-            print("The histogram for this column is now displayed")
-            plt.show()
+            data_dump(df, "pop_apr1")
             choice = None
         elif choice == "2":
-            print(df.pop_jul1.describe())
+            data_dump(df, "pop_apr1")
             choice = None
         elif choice == "3":
-            print(df.delta.describe())
-            plt.hist(df["delta"])
-            plt.show()
+            data_dump(df, "delta")
             choice = None
         elif choice == "4":
             break
         else:
             choice = None
-    
-    #spit out the stats and the histogram
-    #show the menu again
 
 def main_menu():
+    """
+    Parameters: None
+
+    Returns: None
+
+    This function displays a menu to the user, asks for their selection
+    and then calls a function to work a submenu.
+    """
+
     choice = None
-    
+
     while not choice:
         print("Select your choice:")
         print("1. Analyze population data")
@@ -139,10 +155,13 @@ def main_menu():
 
         if choice == "1":
             analyze_population_data()
+            choice = None
         elif choice == "2":
             analyze_housing_data()
+            choice = None
         elif choice == "3":
             break
         else:
             break
+
 main_menu()

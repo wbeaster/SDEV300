b2f7edbc2320008bf2d38b357fd2998dad5a5b56
William Easter <williambeaster@gmail.com>
2020-11-23 20:27:12 +0900

    final

--- week5/lab5/lab5.py
+++ week5/lab5/lab5.py
@@ -10,14 +10,13 @@ import os
 from matplotlib import pyplot as plt
 import pandas as pd
 
-# TODO: use the data without Puerto Rico?
 LOC_POP_CHANGE = "/week5/lab5/PopChange.csv"
 LOC_HOUSING = "/week5/lab5/Housing.csv"
 
-def data_dump(df, column):
+def data_dump(data_frame, column):
     """
     Parameters:
-    df = Pandas data frame with the
+    data_frame = Pandas data frame with the
     column = string of the column whose data will be displayed
 
     Returns: Nothing
@@ -27,8 +26,8 @@ def data_dump(df, column):
     #plt.ticklabel_format(style='plain')
     print(f"You selected {column}")
     print("The statistics for this column are:")
-    print(df[column].describe())
-    plt.hist(df[column], edgecolor='black')
+    print(data_frame[column].describe())
+    plt.hist(data_frame[column], edgecolor='black')
     print("The histogram for this column is now displayed")
     plt.show()
 
@@ -45,10 +44,10 @@ def analyze_housing_data():
     #open the file
     # pd.set_option('precision', 2)
     # pd.set_option('display.float_format', '{:,.2f}'.format)
-    df = pd.read_csv(os.getcwd() + LOC_HOUSING)
+    data_frame = pd.read_csv(os.getcwd() + LOC_HOUSING)
 
     #show a menu giving the user the choice on which column to analyze
-    print("You selected population data")
+    print("You selected housing data")
 
     choice = None
 
@@ -61,37 +60,35 @@ def analyze_housing_data():
         print("5. Analyze 'ROOMS'")
         print("6. Analyze 'WEIGHT'")
         print("7. Analyze 'UTILITY'")
-        print("8. Exit")
+        print("8. Back")
 
         choice = input("Enter your choice (1 - 8): ")
 
         if choice == "1":
-            data_dump(df, "AGE")
+            data_dump(data_frame, "AGE")
             choice = None
         elif choice == "2":
-            data_dump(df, "BEDRMS")
+            data_dump(data_frame, "BEDRMS")
             choice = None
         elif choice == "3":
-            data_dump(df, "BUILT")
+            data_dump(data_frame, "BUILT")
             choice = None
         elif choice == "4":
-            data_dump(df, "NUNITS")
+            data_dump(data_frame, "NUNITS")
             choice = None
         elif choice == "5":
-            data_dump(df, "ROOMS")
+            data_dump(data_frame, "ROOMS")
             choice = None
         elif choice == "6":
-            data_dump(df, "WEIGHT")
+            data_dump(data_frame, "WEIGHT")
             choice = None
         elif choice == "7":
-            data_dump(df, "UTILITY")
+            data_dump(data_frame, "UTILITY")
             choice = None
         elif choice == "8":
-            # TODO: I think we want to make it so just goes up a menu level
             break
         else:
             choice = None
-    return
 
 def analyze_population_data():
     """
@@ -103,11 +100,10 @@ def analyze_population_data():
     and then calls a function to display the appropriate data.
     """
 
-    #TODO: Make it so it does not output exponential notation
     pd.set_option('precision', 2)
     pd.set_option('display.float_format', '{:,.2f}'.format)
-    df = pd.read_csv(os.getcwd() + LOC_POP_CHANGE)
-    df.columns = ["id", "geography", "tgi", "tgi2", "pop_apr1", "pop_jul1", "delta"]
+    data_frame = pd.read_csv(os.getcwd() + LOC_POP_CHANGE)
+    data_frame.columns = ["id", "geography", "tgi", "tgi2", "pop_apr1", "pop_jul1", "delta"]
 
     print("You selected population data")
 
@@ -118,18 +114,18 @@ def analyze_population_data():
         print("1. Analyze 'pop_apr1'")
         print("2. Analyze 'pop_jul1'")
         print("3. Analyze 'delta' (change in population over time)")
-        print("4. Exit")
+        print("4. Back")
 
         choice = input("Enter your choice (1 - 4): ")
 
         if choice == "1":
-            data_dump(df, "pop_apr1")
+            data_dump(data_frame, "pop_apr1")
             choice = None
         elif choice == "2":
-            data_dump(df, "pop_apr1")
+            data_dump(data_frame, "pop_apr1")
             choice = None
         elif choice == "3":
-            data_dump(df, "delta")
+            data_dump(data_frame, "delta")
             choice = None
         elif choice == "4":
             break
@@ -165,6 +161,6 @@ def main_menu():
         elif choice == "3":
             break
         else:
-            break
+            choice = None
 
 main_menu()

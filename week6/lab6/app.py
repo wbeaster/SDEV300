0fa5e83c7af23c1331735d072cb398b2612c1ac1
William Easter <williambeaster@gmail.com>
2020-11-26 21:36:00 +0900

    finished templates and made image files

--- week6/lab6/app.py
+++ week6/lab6/app.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, url_for
 from datetime import datetime
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    #now = datetime.now()
-    return render_template("index.html", now=datetime.now())
+    now = datetime.now()
+    nowstr = now.strftime('%Y-%m-%d %H:%M:%S')
+    return render_template("index.html", nowstr=nowstr)
 
+@app.route('/kualalumpur')
+def kualalumpur():
+    return render_template("kualalumpur.html")
 # TODO: Make routes for the other pages
+
+@app.route('/penang')
+def penang():
+    return render_template("penang.html")
+
+@app.route('/singapore')
+def singapore():
+    return render_template("singapore.html")
+
+@app.route('/vietnam')
+def vietnam():
+    return render_template("vietnam.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-# TODO: Make three links

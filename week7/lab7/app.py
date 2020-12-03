069ca791fada36f621893bba0cc382841037f5b0
William Easter <williambeaster@gmail.com>
2020-12-03 20:53:39 +0900

    routing and security improvements

--- week7/lab7/app.py
+++ week7/lab7/app.py
@@ -4,7 +4,7 @@ places I have travelled to.
 """
 
 from datetime import datetime
-from flask import Flask, render_template, redirect, request, session, url_for
+from flask import Flask, render_template, redirect, request, session, url_for, abort
 
 app = Flask(__name__)
 
@@ -18,53 +18,73 @@ app.secret_key = "changeToFile"
 @app.route('/')
 def index():
     """Serves the homepage"""
+    
+    # Prep the time information
     now = datetime.now()
     nowstr = now.strftime('%Y-%m-%d %H:%M:%S')
-    return render_template("index.html", nowstr=nowstr)
+
+    if "username" in session:
+        return render_template("index.html", nowstr=nowstr)
+    else:
+        return redirect(url_for("login"))
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        login_name = request.form["nm"]
-        session["login_name"] = login_name
-        return redirect(url_for("user"))
+        username = request.form["nm"]
+        session["username"] = username
+        return redirect(url_for("index"))
     else:
-        if "login_name" in session:
-            return redirect(url_for("user"))
+        if "username" in session:
+            return redirect(url_for("index"))
         return render_template("login.html")
 
 @app.route("/logout")
 def logout():
-    session.pop("login_name", None)
-    return redirect(url_for("login"))
+    session.pop("username", None)
+    return redirect(url_for("index"))
 
 @app.route('/user')
 def user():
-    if "login_name" in session:
-        login_name = session["login_name"]
-        return f"<h1>{login_name}</h1>"
+    if "username" in session:
+        username = session["username"]
+        return f"<h1>{username}</h1>"
     else:
         return redirect(url_for("login"))
 
 @app.route('/kualalumpur')
 def kualalumpur():
     """Serves the Kuala Lumpur page"""
-    return render_template("kualalumpur.html")
+    if "username" in session:
+        return render_template("kualalumpur.html")
+    else:
+        abort(403)
 
 @app.route('/penang')
 def penang():
     """Serves the Penang page"""
-    return render_template("penang.html")
+    if "username" in session:
+        return render_template("penang.html")
+    else:
+        abort(403)
+
 
 @app.route('/singapore')
 def singapore():
     """Serves the Singapore page"""
-    return render_template("singapore.html")
+    if "username" in session:
+        return render_template("singapore.html")
+    else:
+        abort(403)
+
 
 @app.route('/vietnam')
 def vietnam():
     """Serves the Vietnam page"""
-    return render_template("vietnam.html")
+    if "username" in session:
+        return render_template("vietnam.html")
+    else:
+        abort(403)
 
 if __name__ == '__main__':
     app.run(debug=True)

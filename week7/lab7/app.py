79cc446a82abd942ea2b0728bc01ef173637fdc6
William Easter <williambeaster@gmail.com>
2020-12-03 23:22:35 +0900

    may have to redo the virtual environment

--- week7/lab7/app.py
+++ week7/lab7/app.py
@@ -5,6 +5,14 @@ places I have travelled to.
 
 from datetime import datetime
 from flask import Flask, render_template, redirect, request, session, url_for, abort
+from os.path import exists
+from passlib.hash import sha256_crypt
+
+# TODO: Need to make a new virtual environment, not just copy and paste from lab6
+# TODO: Make a constant for the name of the password file
+# PSsfile format: username, salt, password hash
+PASSFILE = "passfile"
+KEYFILE = "keyfile"
 
 app = Flask(__name__)
 
@@ -14,6 +22,9 @@ app.secret_key = "changeToFile"
 #Uncomment the below line to not have to check a file for the secret
 #key. Secret ket normally stored in a file that is not uploaded to github
 #app.secret_key = "changeToFile"
+
+if not exists(PASSFILE):
+    open(PASSFILE, "w").close()
 
 @app.route('/')
 def index():
@@ -31,7 +42,9 @@ def index():
 @app.route('/login', methods=["POST", "GET"])
 def login():
     if request.method == "POST":
-        username = request.form["nm"]
+        username = request.form["username"]
+        #TODO: Write the code to check the passfile
+        
         session["username"] = username
         return redirect(url_for("index"))
     else:
@@ -43,6 +56,32 @@ def login():
 def logout():
     session.pop("username", None)
     return redirect(url_for("index"))
+
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # TODO: salt = ???
+        salt = "a"
+        password_hash = sha256_crypt.hash(password + salt)
+
+        # TODO: Use flash to highlight registration errors
+
+        # open file
+        with open(PASSFILE, "a") as f:
+            f.writelines(username + ", " + salt + "," + password_hash)
+        # write date to file
+        # close file
+        # show success screen
+        # go to login
+    
+    if "username" in session:
+        # TODO: Show them a special page
+        pass
+    else:
+        return render_template("register.html")
+
 
 @app.route('/user')
 def user():
@@ -58,7 +97,7 @@ def kualalumpur():
     if "username" in session:
         return render_template("kualalumpur.html")
     else:
-        abort(403)
+        abort(401)
 
 @app.route('/penang')
 def penang():
@@ -66,7 +105,7 @@ def penang():
     if "username" in session:
         return render_template("penang.html")
     else:
-        abort(403)
+        abort(401)
 
 
 @app.route('/singapore')
@@ -75,7 +114,7 @@ def singapore():
     if "username" in session:
         return render_template("singapore.html")
     else:
-        abort(403)
+        abort(401)
 
 
 @app.route('/vietnam')
@@ -84,7 +123,7 @@ def vietnam():
     if "username" in session:
         return render_template("vietnam.html")
     else:
-        abort(403)
+        abort(401)
 
 if __name__ == '__main__':
     app.run(debug=True)

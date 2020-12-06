51e29adae7663062ec234537abc125f1a90f57e5
William Easter <williambeaster@gmail.com>
2020-12-06 22:32:33 +0900

    near final

--- week7/lab7a/app.py
+++ week7/lab7a/app.py
@@ -4,49 +4,63 @@ places I have travelled to.
 """
 
 from datetime import datetime
-from flask import Flask, flash, render_template, redirect, request, session, url_for, abort
 from os.path import exists
-from passlib.hash import sha256_crypt
 from string import punctuation
+from flask import Flask, flash, render_template, redirect, request, session, url_for, abort
+from passlib.hash import sha256_crypt
 
-# TODO: Need to make a new virtual environment, not just copy and paste from lab6
-# TODO: Make a constant for the name of the password file
-# PSsfile format: username, salt, password hash
+# passfile format: username password hash
 PASSFILE = "passfile"
+
+# location of the Flask app.secret_key
 KEYFILE = "keyfile"
 
 app = Flask(__name__)
 app.debug = True
 
-# TODO: Change this to a file
-app.secret_key = "changeToFile"
+# comment the two line below if there is no seperate key file
+# in the next block you can uncomment a line to use a weak key
+with open(KEYFILE, "r") as keyfile:
+    app.secret_key = keyfile.readlines()[0]
 
-#Uncomment the below line to not have to check a file for the secret
-#key. Secret ket normally stored in a file that is not uploaded to github
-#app.secret_key = "changeToFile"
+# Uncomment the below line to not have to check a file for the secret
+# key. Secret ket normally stored in a file that is not uploaded to github
+# app.secret_key = "changeToFile"
 
 # create a file for logins and hashes if it does not exist
 if not exists(PASSFILE):
     open(PASSFILE, "w").close()
 
 def is_registered(username):
+    """
+    Function checks to see if the user already exists in PASSFILE
+    """
     with open(PASSFILE, "r") as passfile:
         for record in passfile:
             r_username, r_salt_hash = record.split()
+            # The below is just for the linter
+            r_salt_hash = r_salt_hash + "nothing"
             if username == r_username:
                 return True
     return False
 
 def has_whitespace(string):
+    """
+    Function determines if a string, probably a username, has white space
+    in it.
+    """
     temp = string.split()
     return len(temp) > 1
 
 def is_complex(password):
-    if (len(password) >= 12):
-        if (any(c.isupper() for c in password)):
-            if (any(c.islower() for c in password)):
-                if (any(c.isdigit() for c in password)):
-                    if (any(c in punctuation for c in password)):
+    """
+    Function determines if a password meets the programs complexity requirements
+    """
+    if len(password) >= 12:
+        if any(c.isupper() for c in password):
+            if any(c.islower() for c in password):
+                if any(c.isdigit() for c in password):
+                    if any(c in punctuation for c in password):
                         return True
     return False
 
@@ -59,15 +73,15 @@ def index():
 
     if "username" in session:
         return render_template("index.html", nowstr=nowstr)
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    """Serves the login page"""
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-        
+
         valid_user = False
         valid_password = False
 
@@ -90,21 +104,23 @@ def login():
     else:
         if "username" in session:
             return redirect(url_for("index"))
-    
+
     return render_template("login.html")
 
 @app.route("/logout")
 def logout():
+    """Serves the logout page"""
     session.pop("username", None)
     flash("You have been logged out.")
     return redirect(url_for("index"))
 
 @app.route("/register", methods=["POST", "GET"])
 def register():
+    """Serve the register page."""
     if request.method == "POST":
         if "username" in session:
             flash("Logout if you would like a new registration.")
-    
+
         username = None
         password = None
         error = None
@@ -130,40 +146,37 @@ def register():
                 passfile.write(username + " " + password_hash + "\n")
             flash("Registration successful. Please login.")
             return redirect(url_for("login"))
-    
+
     return render_template("register.html")
 
 @app.route('/user')
 def user():
+    """Serves a page with the user's name on it."""
     if "username" in session:
         username = session["username"]
         return f"<h1>{username}</h1>"
-    else:
-        return redirect(url_for("login"))
+    return redirect(url_for("login"))
 
 @app.route('/kualalumpur')
 def kualalumpur():
     """Serves the Kuala Lumpur page"""
     if "username" in session:
         return render_template("kualalumpur.html")
-    else:
-        abort(401)
+    return abort(401)
 
 @app.route('/penang')
 def penang():
     """Serves the Penang page"""
     if "username" in session:
         return render_template("penang.html")
-    else:
-        abort(401)
+    return abort(401)
 
 @app.route('/singapore')
 def singapore():
     """Serves the Singapore page"""
     if "username" in session:
         return render_template("singapore.html")
-    else:
-        abort(401)
+    return abort(401)
 
 
 @app.route('/vietnam')
@@ -171,16 +184,14 @@ def vietnam():
     """Serves the Vietnam page"""
     if "username" in session:
         return render_template("vietnam.html")
-    else:
-        abort(401)
+    return abort(401)
 
 @app.route('/table')
 def table():
     """Serves a table of pictures"""
     if "username" in session:
         return render_template("table.html")
-    else:
-        abort(401)
+    return abort(401)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
